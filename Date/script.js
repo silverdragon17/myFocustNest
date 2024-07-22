ecc6435b507ea457981a98d979b2200e42467f32
Adi <contact@adihafiz.com>
2024-07-22 18:37:16 +0800

    Update script.js

--- Date/script.js
+++ Date/script.js
@@ -4,8 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (dateDisplayElement && dateLabelElement) {
         function formatDate(date) {
-            //const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-            //const dayName = days[date.getDay()];
             const day = String(date.getDate()).padStart(2, '0');
             const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
             const year = date.getFullYear();
