a87dffc4746a92ef2e3f0261d9b2f58d9ee75bbb
Adi <contact@adihafiz.com>
2024-07-22 16:08:21 +0800

    Update script.js

--- Date/script.js
+++ Date/script.js
@@ -9,7 +9,8 @@ document.addEventListener("DOMContentLoaded", () => {
             const day = String(date.getDate()).padStart(2, '0');
             const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
             const year = date.getFullYear();
-            return `$dayName, ${day}/${month}/${year}`;
+            return `${dayName}';
+            return '${day}/${month}/${year}';
         }
 
         function updateDateDisplay() {
